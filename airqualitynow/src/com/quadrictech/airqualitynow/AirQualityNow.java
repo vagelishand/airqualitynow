a1a342d6e9fd7159d8c27f8023e397ccb81ec934
artgonzalez <artgonzalezjr@gmail.com>
2011-08-04 15:39:38 -0500

    change file name

--- airqualitynow/src/com/quadrictech/airqualitynow/AirQualityNow.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/AirQualityNow.java
@@ -3,7 +3,7 @@ package com.quadrictech.airqualitynow;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class airqualitynow extends Activity {
+public class AirQualityNow extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
