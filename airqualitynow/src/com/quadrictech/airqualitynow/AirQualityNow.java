2502a5e8b97ba2418cda0c8d3e354d01325aa322
artgonzalez <artgonzalezjr@gmail.com>
2011-08-05 01:23:30 -0500

    graphical user interface design

--- airqualitynow/src/com/quadrictech/airqualitynow/AirQualityNow.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/AirQualityNow.java
@@ -1,9 +1,14 @@
 package com.quadrictech.airqualitynow;
 
-import roboguice.activity.RoboActivity;
+import com.google.inject.Inject;
+import com.quadrictech.airqualitynow.forecast.AQIForecastView;
+
+import roboguice.activity.RoboTabActivity;
 import android.os.Bundle;
 
-public class AirQualityNow extends RoboActivity {
+public class AirQualityNow extends RoboTabActivity {
+	@Inject private AQIForecastView AirQualityNowView;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
