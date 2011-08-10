906e9a649e2830953af67af0f8cbd42ee063b0a4
artgonzalez <artgonzalezjr@gmail.com>
2011-08-10 17:02:15 -0500

    add onitemclick and onclick methods

--- airqualitynow/src/com/quadrictech/airqualitynow/view/IForecastListView.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/view/IForecastListView.java
@@ -1,5 +1,8 @@
 package com.quadrictech.airqualitynow.view;
 
+import android.view.View;
+import android.widget.AdapterView;
+
 public interface IForecastListView<T> extends IView<T> {
-	
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id);
 }
