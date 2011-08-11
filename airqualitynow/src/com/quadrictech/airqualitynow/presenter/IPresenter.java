bc2dd4c3de7a62bcf657cd668eb17bb9b046a1ee
artgonzalez <artgonzalezjr@gmail.com>
2011-08-11 01:15:19 -0500

    method moved to iforecastlistpresenter

--- airqualitynow/src/com/quadrictech/airqualitynow/presenter/IPresenter.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/presenter/IPresenter.java
@@ -1,8 +1,5 @@
 package com.quadrictech.airqualitynow.presenter;
 
-import com.j256.ormlite.support.ConnectionSource;
-
 public interface IPresenter<T> {
-	public void initialize(T view, ConnectionSource connectionSource);
 	public void onDestroy();
 }
