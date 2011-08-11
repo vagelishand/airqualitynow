ba6d3a78751cdf747a980c60db23e513eab7857e
artgonzalez <artgonzalezjr@gmail.com>
2011-08-11 01:13:12 -0500

    add more bindings ... typeliteral required

--- airqualitynow/src/com/quadrictech/airqualitynow/robo/AirQualityNowApplicationModule.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/robo/AirQualityNowApplicationModule.java
@@ -1,18 +1,27 @@
 package com.quadrictech.airqualitynow.robo;
 
 
+import android.view.View;
+
 import com.google.inject.AbstractModule;
+import com.google.inject.TypeLiteral;
 import com.quadrictech.airqualitynow.db.ForecastRepository;
 import com.quadrictech.airqualitynow.db.IForecastRepository;
 import com.quadrictech.airqualitynow.presenter.ForecastListPresenter;
+import com.quadrictech.airqualitynow.presenter.ForecastPresenter;
 import com.quadrictech.airqualitynow.presenter.IForecastListPresenter;
+import com.quadrictech.airqualitynow.presenter.IForecastPresenter;
 import com.quadrictech.airqualitynow.view.ForecastListView;
+import com.quadrictech.airqualitynow.view.ForecastView;
 import com.quadrictech.airqualitynow.view.IForecastListView;
+import com.quadrictech.airqualitynow.view.IForecastView;
 
 public class AirQualityNowApplicationModule extends AbstractModule{
 
 	@Override
 	protected void configure() {
+		bind(new TypeLiteral <IForecastPresenter<View>>(){}).to((Class<? extends IForecastPresenter<View>>) ForecastPresenter.class);
+		bind(new TypeLiteral<IForecastView<View>>(){}).to(ForecastView.class);
 		bind(IForecastListPresenter.class).to(ForecastListPresenter.class);
 		bind(IForecastListView.class).to(ForecastListView.class);
 		bind(IForecastRepository.class).to(ForecastRepository.class);
