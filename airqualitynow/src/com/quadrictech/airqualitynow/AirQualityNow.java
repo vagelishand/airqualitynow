503ea8f753ff73193ba50f5a72301e4904b41953
artgonzalez <artgonzalezjr@gmail.com>
2011-08-11 01:17:22 -0500

    make this main app menu

--- airqualitynow/src/com/quadrictech/airqualitynow/AirQualityNow.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/AirQualityNow.java
@@ -1,51 +1,18 @@
 package com.quadrictech.airqualitynow;
 
-import com.google.inject.Inject;
-import com.j256.ormlite.support.ConnectionSource;
-import com.quadrictech.airqualitynow.db.DatabaseHelper;
-import com.quadrictech.airqualitynow.presenter.ForecastListPresenter;
-import com.quadrictech.airqualitynow.service.DataProviderService;
-import com.quadrictech.airqualitynow.view.ForecastListView;
-
 import roboguice.activity.RoboActivity;
-import android.content.Intent;
 import android.os.Bundle;
 
 public class AirQualityNow extends RoboActivity {
-	@Inject private ForecastListView mForecastListView;
-	@Inject private ForecastListPresenter mForecastListPresenter;
-	DatabaseHelper helper;
-	private ConnectionSource mConnectionSource;
-	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        Intent intent = new Intent(this, DataProviderService.class);
-        startService(intent);
-        helper = new DatabaseHelper(this);
-        mConnectionSource = helper.getConnectionSource();
-        initializeViews();
-        initializePresenters();
-    }
-    
-    private void initializeViews(){
-    	mForecastListView.initialize();
-    }
-    
-    private void initializePresenters(){
-    	mForecastListView.mPresenter = mForecastListPresenter;
-    	mForecastListPresenter.initialize(mForecastListView, mConnectionSource);
     }
     
     @Override
     public void onDestroy(){
     	super.onDestroy();
-    	Intent intent = new Intent(this, DataProviderService.class);
-    	stopService(intent);
-    	mForecastListView.onDestroy();
-    	mForecastListPresenter.onDestroy();
     }
 }
