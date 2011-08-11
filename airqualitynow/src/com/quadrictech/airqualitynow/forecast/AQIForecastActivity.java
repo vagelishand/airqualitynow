29aa85abfc014acf706172d2874160e9cd943dac
artgonzalez <artgonzalezjr@gmail.com>
2011-08-11 01:17:10 -0500

    init views n presenters

--- airqualitynow/src/com/quadrictech/airqualitynow/forecast/AQIForecastActivity.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/forecast/AQIForecastActivity.java
@@ -1,17 +1,33 @@
 package com.quadrictech.airqualitynow.forecast;
 
-import android.os.Bundle;
+import com.google.inject.Inject;
 
 import com.quadrictech.airqualitynow.R;
+import com.quadrictech.airqualitynow.presenter.IForecastPresenter;
+import com.quadrictech.airqualitynow.view.IForecastView;
 
 import roboguice.activity.RoboActivity;
+import android.os.Bundle;
+import android.view.View;
 
 public class AQIForecastActivity extends RoboActivity {
+	@Inject private IForecastView<View> mForecastView;
+	@Inject private IForecastPresenter<View> mForecastPresenter;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forecast);
+        
+        mForecastView.initialize(mForecastPresenter);
+    	mForecastPresenter.initialize(mForecastView);
     }
-
+    
+    @Override
+    public void onDestroy(){
+    	super.onDestroy();
+    	mForecastView.onDestroy();
+    	mForecastPresenter.onDestroy();
+    }
 }
--- airqualitynow/src/com/quadrictech/airqualitynow/forecast/AQIForecastListActivity.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/forecast/AQIForecastListActivity.java
@@ -1,17 +1,52 @@
 package com.quadrictech.airqualitynow.forecast;
 
-import android.os.Bundle;
-
+import com.google.inject.Inject;
+import com.j256.ormlite.support.ConnectionSource;
 import com.quadrictech.airqualitynow.R;
+import com.quadrictech.airqualitynow.db.DatabaseHelper;
+import com.quadrictech.airqualitynow.presenter.ForecastListPresenter;
+import com.quadrictech.airqualitynow.service.DataProviderService;
+import com.quadrictech.airqualitynow.view.ForecastListView;
 
 import roboguice.activity.RoboActivity;
+import android.content.Intent;
+import android.os.Bundle;
 
 public class AQIForecastListActivity extends RoboActivity {
+	@Inject private ForecastListView mForecastListView;
+	@Inject private ForecastListPresenter mForecastListPresenter;
+	DatabaseHelper helper;
+	private ConnectionSource mConnectionSource;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forecastlist);
+        
+        Intent intent = new Intent(this, DataProviderService.class);
+        startService(intent);
+        helper = new DatabaseHelper(this);
+        mConnectionSource = helper.getConnectionSource();
+        initializeViews();
+        initializePresenters();
     }
-
+    
+    private void initializeViews(){
+    	mForecastListView.initialize();
+    }
+    
+    private void initializePresenters(){
+    	mForecastListView.mPresenter = mForecastListPresenter;
+    	mForecastListPresenter.initialize(mForecastListView, mConnectionSource);
+    }
+    
+    @Override
+    public void onDestroy(){
+    	super.onDestroy();
+    	Intent intent = new Intent(this, DataProviderService.class);
+    	stopService(intent);
+    	mForecastListView.onDestroy();
+    	mForecastListPresenter.onDestroy();
+    }
 }
