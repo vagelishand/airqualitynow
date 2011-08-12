c10ad1e370eb5b272c3870c327ca55ad96740eef
artgonzalez <artgonzalezjr@gmail.com>
2011-08-12 01:51:11 -0500

    test httpget w GoogleUrl

--- airqualitynowTest/src/com/quadrictech/airqualitynow/test/inet/rest/RestClientTest.java
+++ airqualitynowTest/src/com/quadrictech/airqualitynow/test/inet/rest/RestClientTest.java
@@ -2,9 +2,11 @@ package com.quadrictech.airqualitynow.test.inet.rest;
 
 import roboguice.test.RoboUnitTestCase;
 
+import com.google.api.client.googleapis.GoogleUrl;
 import android.test.suitebuilder.annotation.MediumTest;
-import android.util.Log;
 
+import com.quadrictech.airqualitynow.inet.callback.ForecastRequestCallback;
+import com.quadrictech.airqualitynow.inet.callback.IRequestCallback;
 import com.quadrictech.airqualitynow.inet.rest.RestClient;
 import com.quadrictech.airqualitynow.robo.AirQualityNowApplication;
 
@@ -17,7 +19,21 @@ public class RestClientTest extends RoboUnitTestCase<AirQualityNowApplication>{
 	}
 	
 	@MediumTest
-	public void testExecuteHttpGet(){//654C8A3C-683A-43FD-BBB2-B1F45B8F1E21
-		client.executeHttpGet("http://ws1.airnowgateway.org/GatewayWebServiceREST/Gateway.svc/forecastbyzipcode?zipcode=94954&date=2011-08-11&format=json&key=654C8A3C-683A-43FD-BBB2-B1F45B8F1E21", null);
+	public void testExecuteHttpGet(){
+		String zip = "78756";
+		String date = "2011-08-11";
+		String format = "json";
+		String key = "654C8A3C-683A-43FD-BBB2-B1F45B8F1E21";
+		
+		StringBuilder url = new StringBuilder();
+		url.append("http://ws1.airnowgateway.org/GatewayWebServiceREST/Gateway.svc/forecastbyzipcode?");
+		url.append("zipcode=" + zip);
+		url.append("&data=" + date);
+		url.append("&format=" + format);
+		url.append("&key=" + key);
+		
+		GoogleUrl googleUrl = new GoogleUrl(url.toString());
+		IRequestCallback callback = new ForecastRequestCallback();
+		client.executeHttpGet(googleUrl, callback);
 	}
 }
