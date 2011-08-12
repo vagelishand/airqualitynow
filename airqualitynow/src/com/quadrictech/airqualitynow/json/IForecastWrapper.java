4aaa1297a77455d2ddbbac38085a78e705562cce
artgonzalez <artgonzalezjr@gmail.com>
2011-08-12 15:25:26 -0500

    remove inline class pacakage identificaiton

--- airqualitynow/src/com/quadrictech/airqualitynow/json/IForecastWrapper.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/json/IForecastWrapper.java
@@ -2,7 +2,9 @@ package com.quadrictech.airqualitynow.json;
 
 import java.util.List;
 
+import com.quadrictech.airqualitynow.model.Forecast;
+
 public interface IForecastWrapper {
-	public List<com.quadrictech.airqualitynow.model.Forecast> getForecast();
-	public void setForecast(List<com.quadrictech.airqualitynow.model.Forecast> forecasts);
+	public List<Forecast> getForecast();
+	public void setForecast(List<Forecast> forecasts);
 }
