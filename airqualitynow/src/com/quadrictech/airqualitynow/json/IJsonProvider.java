91a682785a4c7e0101afae8cbd449f111594b88a
artgonzalez <artgonzalezjr@gmail.com>
2011-08-12 16:09:54 -0500

    parametrize class and provide default method to be overwritten

--- airqualitynow/src/com/quadrictech/airqualitynow/json/IJsonProvider.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/json/IJsonProvider.java
@@ -1,5 +1,7 @@
 package com.quadrictech.airqualitynow.json;
 
-public interface IJsonProvider {
-	
+import org.codehaus.jackson.map.ObjectMapper;
+
+public interface IJsonProvider<T> {
+	public T parseJson(ObjectMapper mapper, String json);
 }
