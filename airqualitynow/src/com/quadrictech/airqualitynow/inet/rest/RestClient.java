6e2449f723a0d13788e8bbd19416051256b7e02f
artgonzalez <artgonzalezjr@gmail.com>
2011-08-12 01:49:32 -0500

    update to use IRequestCallback for success or failure handling

--- airqualitynow/src/com/quadrictech/airqualitynow/inet/rest/RestClient.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/inet/rest/RestClient.java
@@ -1,27 +1,29 @@
 package com.quadrictech.airqualitynow.inet.rest;
 
 import java.io.IOException;
-import com.google.api.client.http.GenericUrl;
+
+import com.google.api.client.googleapis.GoogleUrl;
 import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.extensions.android2.AndroidHttp;
+import com.quadrictech.airqualitynow.inet.callback.IRequestCallback;
 
 public class RestClient {
 	public static final HttpTransport TRANSPORT = (HttpTransport) AndroidHttp.newCompatibleTransport();
 	
-	public void executeHttpGet(String url, String acceptValue){
+	public void executeHttpGet(GoogleUrl url, IRequestCallback requestCallback){
 		HttpRequest request = null;
+		
 		try {
-			request = TRANSPORT.createRequestFactory().buildGetRequest(new GenericUrl(url));
+			request = TRANSPORT.createRequestFactory().buildGetRequest(url);
 			
-			String s = request.execute().parseAsString();
+			HttpResponse response = request.execute();
 			
-			//create callback object to process response
+			requestCallback.onResponseReceived(response);
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			//process error in a callback object
-			e.printStackTrace();
+			requestCallback.onError(new Throwable("Error getting remote data."));			
 		}
 		finally{
 			try {
@@ -31,7 +33,5 @@ public class RestClient {
 				e.printStackTrace();
 			}
 		}
-		
 	}
-
 }
