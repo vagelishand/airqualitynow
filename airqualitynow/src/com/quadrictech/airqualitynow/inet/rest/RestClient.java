c600a05c06358c1bc8a8514ceb57510047606a63
artgonzalez <artgonzalezjr@gmail.com>
2011-08-12 13:07:00 -0500

    update executeHttpGet to process HttpResponse using an IRequestCallback

--- airqualitynow/src/com/quadrictech/airqualitynow/inet/rest/RestClient.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/inet/rest/RestClient.java
@@ -10,28 +10,53 @@ import com.google.api.client.extensions.android2.AndroidHttp;
 import com.quadrictech.airqualitynow.inet.callback.IRequestCallback;
 
 public class RestClient {
+	/**
+	 * This should return a compatible HttpTransport for current device
+	 */
 	public static final HttpTransport TRANSPORT = (HttpTransport) AndroidHttp.newCompatibleTransport();
+	private boolean mError;
 	
+	/**
+	 * Execute Http Get requests using google apis
+	 * 
+	 * @param url Callers supply a url string with get parameters
+	 * @param requestCallback Uses to process reply/error 
+	 */
 	public void executeHttpGet(GoogleUrl url, IRequestCallback requestCallback){
 		HttpRequest request = null;
+		HttpResponse response = null;
 		
 		try {
 			request = TRANSPORT.createRequestFactory().buildGetRequest(url);
 			
-			HttpResponse response = request.execute();
+			response = request.execute();
 			
 			requestCallback.onResponseReceived(response);
-			
+			mError = false;
 		} catch (IOException e) {
-			requestCallback.onError(new Throwable("Error getting remote data."));			
+			requestCallback.onError(new Throwable("Error getting remote data."));
+			mError = true;
 		}
 		finally{
 			try {
+				if(response != null){
+					response = null;
+				}
+				
 				TRANSPORT.shutdown();
+				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	/**
+	 * This is mainly used in the Test project for Unit Testing
+	 * @return Request execute error status
+	 */
+	public boolean getErrorStatus(){
+		return mError;
 	}
 }
