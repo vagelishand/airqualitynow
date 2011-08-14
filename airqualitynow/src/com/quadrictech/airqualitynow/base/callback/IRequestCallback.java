d539adc0330e09271bda058220d55941d305eb62
artgonzalez <artgonzalezjr@gmail.com>
2011-08-14 16:29:33 -0500

    parametrize to use for local n remote callbacks

---
+++ airqualitynow/src/com/quadrictech/airqualitynow/base/callback/IRequestCallback.java
@@ -0,0 +1,11 @@
+package com.quadrictech.airqualitynow.base.callback;
+
+/**
+ * Interface for updating local db or reporting an error received from remote DB
+ * @author Art Gonzalez
+ *
+ */
+public interface IRequestCallback<T> {
+	public void onError(Throwable exception);
+	public void onResponseReceived(T response);
+}
