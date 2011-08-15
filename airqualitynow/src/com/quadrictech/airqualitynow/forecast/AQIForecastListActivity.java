72bd8a0f994512746192d9e244f998594670f840
artgonzalez <artgonzalezjr@gmail.com>
2011-08-15 01:00:04 -0500

    working w event manager

--- airqualitynow/src/com/quadrictech/airqualitynow/forecast/AQIForecastListActivity.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/forecast/AQIForecastListActivity.java
@@ -5,18 +5,24 @@ import com.j256.ormlite.support.ConnectionSource;
 import com.quadrictech.airqualitynow.R;
 import com.quadrictech.airqualitynow.db.DatabaseHelper;
 import com.quadrictech.airqualitynow.presenter.ForecastListPresenter;
+import com.quadrictech.airqualitynow.presenter.PresenterInitializeParameter;
 import com.quadrictech.airqualitynow.service.DataProviderService;
+import com.quadrictech.airqualitynow.service.helper.DataProviderServiceHelper;
+import com.quadrictech.airqualitynow.service.helper.IDataProviderServiceHelper;
 import com.quadrictech.airqualitynow.view.ForecastListView;
 
 import roboguice.activity.RoboActivity;
+import roboguice.event.EventManager;
 import android.content.Intent;
 import android.os.Bundle;
 
 public class AQIForecastListActivity extends RoboActivity {
 	@Inject private ForecastListView mForecastListView;
 	@Inject private ForecastListPresenter mForecastListPresenter;
+	@Inject protected EventManager mEventManager;
 	DatabaseHelper helper;
 	private ConnectionSource mConnectionSource;
+	private IDataProviderServiceHelper serviceHelper;
 	
     /** Called when the activity is first created. */
     @Override
@@ -24,8 +30,9 @@ public class AQIForecastListActivity extends RoboActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forecastlist);
         
-        Intent intent = new Intent(this, DataProviderService.class);
+        Intent intent = new Intent(this.getApplicationContext(), DataProviderService.class);
         startService(intent);
+        serviceHelper = new DataProviderServiceHelper(this.getApplicationContext(), mEventManager);
         helper = new DatabaseHelper(this);
         mConnectionSource = helper.getConnectionSource();
         initializeViews();
@@ -38,7 +45,7 @@ public class AQIForecastListActivity extends RoboActivity {
     
     private void initializePresenters(){
     	mForecastListView.mPresenter = mForecastListPresenter;
-    	mForecastListPresenter.initialize(mForecastListView, mConnectionSource);
+    	mForecastListPresenter.initialize(new PresenterInitializeParameter(mForecastListView, mConnectionSource, mEventManager, (DataProviderServiceHelper) this.serviceHelper));
     }
     
     @Override
