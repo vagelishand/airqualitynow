ebc993b60a8465f6718c8dfd183df3148849ac7b
artgonzalez <artgonzalezjr@gmail.com>
2011-08-15 00:58:49 -0500

    add presenter parameter object

--- airqualitynow/src/com/quadrictech/airqualitynow/presenter/IForecastListPresenter.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/presenter/IForecastListPresenter.java
@@ -1,12 +1,7 @@
 package com.quadrictech.airqualitynow.presenter;
 
-import android.widget.ListView;
-
-import com.j256.ormlite.support.ConnectionSource;
-import com.quadrictech.airqualitynow.view.IForecastListView;
-
 public interface IForecastListPresenter<T> extends IPresenter<T>{
-	public void initialize(IForecastListView<ListView> view, ConnectionSource connectionSource);
+	public void initialize(PresenterInitializeParameter parameterObject);
 	public void initializeList();
 	public void onPollutantGuideButtonClick();
 	public void onSearchAreaClick();
