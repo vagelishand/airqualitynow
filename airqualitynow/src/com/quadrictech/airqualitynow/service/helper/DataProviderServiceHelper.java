5eef4425c62182891e073f4bc48185bd3cc9081e
artgonzalez <artgonzalezjr@gmail.com>
2011-08-15 01:01:43 -0500

    event management to communicate w presenter n service

--- airqualitynow/src/com/quadrictech/airqualitynow/service/helper/DataProviderServiceHelper.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/service/helper/DataProviderServiceHelper.java
@@ -1,19 +1,72 @@
 package com.quadrictech.airqualitynow.service.helper;
 
-import java.util.List;
+import roboguice.event.EventManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.widget.Toast;
 
-import com.quadrictech.airqualitynow.model.Forecast;
+import com.quadrictech.airqualitynow.base.callback.IDisposable;
+import com.quadrictech.airqualitynow.db.callback.ForecastRequestCallback;
+import com.quadrictech.airqualitynow.service.DataProviderService;
 
-public class DataProviderServiceHelper implements IDataProviderServiceHelper{
-
-	public List<Forecast> getAllForecasts() {
-		// TODO Auto-generated method stub
-		return null;
+public class DataProviderServiceHelper implements IDataProviderServiceHelper, IDisposable{
+	private Context mContext;
+	private boolean mServiceBound;
+	private EventManager mEventManager;
+	
+	public DataProviderServiceHelper(){
+		
+	}
+	
+	public DataProviderServiceHelper(Context context, EventManager eventManager){
+		mContext = context;
+		mEventManager = eventManager;
+		doBindService();
+	}
+	
+	public void getAllForecasts() {
+		//IForecastRequestCallback callback= mDataServiceProvider.onGetAllReportingAreas();
+		mEventManager.fire(mContext, new ForecastRequestCallback());
 	}
 
-	public Forecast getForecastById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public void getForecastById(int id) {
 	}
 
+	public void getAllReportingAreas() {
+		
+	}
+	
+	private DataProviderService mDataServiceProvider;
+	
+	private ServiceConnection mConnection = new ServiceConnection(){
+
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			 Toast.makeText(mContext, "connected", Toast.LENGTH_SHORT).show();
+			mDataServiceProvider = ((DataProviderService.LocalBinder)service).getService();			
+		}
+
+		public void onServiceDisconnected(ComponentName className) {
+			mDataServiceProvider = null;			
+		}
+		
+	};
+
+	public void doBindService() {
+		mServiceBound = mContext.bindService(new Intent(mContext, DataProviderService.class), mConnection, Context.BIND_AUTO_CREATE);		
+	}
+
+	public void doUnBindService() {
+		if(mServiceBound){
+			mContext.unbindService(mConnection);
+			mServiceBound = false;
+		}
+	}
+
+	public void onDestroy() {
+		doUnBindService();		
+	}
+	
 }
--- airqualitynow/src/com/quadrictech/airqualitynow/service/helper/IDataProviderServiceHelper.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/service/helper/IDataProviderServiceHelper.java
@@ -1,12 +1,10 @@
 package com.quadrictech.airqualitynow.service.helper;
 
-import java.util.List;
-
-import com.quadrictech.airqualitynow.model.Forecast;
-import com.quadrictech.airqualitynow.model.ReportingArea;
 
 public interface IDataProviderServiceHelper extends IServiceHelper {
-	public List<Forecast> getAllForecasts();
-	public Forecast getForecastById(int id);
-	//public List<ReportingArea> getAllReportingAreas();
+	public void getAllForecasts();
+	public void getForecastById(int id);
+	public void getAllReportingAreas();
+	public void doBindService();
+	public void doUnBindService();
 }
