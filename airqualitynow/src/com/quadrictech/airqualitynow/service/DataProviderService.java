89827db5cf0c4c072011b37c71cc52593b3bf9d6
artgonzalez <artgonzalezjr@gmail.com>
2011-08-15 01:00:48 -0500

    add service binding and repository method

--- airqualitynow/src/com/quadrictech/airqualitynow/service/DataProviderService.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/service/DataProviderService.java
@@ -1,19 +1,47 @@
 package com.quadrictech.airqualitynow.service;
 
 import java.sql.SQLException;
+import java.util.List;
+
 
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 import com.j256.ormlite.android.apptools.OrmLiteBaseService;
+import com.quadrictech.airqualitynow.base.callback.IRequestCallback;
+import com.quadrictech.airqualitynow.db.AppRepository;
 import com.quadrictech.airqualitynow.db.DatabaseHelper;
+import com.quadrictech.airqualitynow.db.ForecastRepository;
+import com.quadrictech.airqualitynow.db.IAppRepository;
+import com.quadrictech.airqualitynow.db.IForecastRepository;
+import com.quadrictech.airqualitynow.db.IReportingAreaRepository;
+import com.quadrictech.airqualitynow.db.IRepository;
+import com.quadrictech.airqualitynow.db.ReportingAreaRepository;
+import com.quadrictech.airqualitynow.db.Repository;
+import com.quadrictech.airqualitynow.db.callback.ForecastRequestCallback;
+import com.quadrictech.airqualitynow.db.callback.IForecastRequestCallback;
+import com.quadrictech.airqualitynow.model.IReportingAreaWrapper;
+import com.quadrictech.airqualitynow.model.ReportingArea;
 import com.quadrictech.airqualitynow.model.State;
 
 public class DataProviderService extends OrmLiteBaseService<DatabaseHelper> {
+	/**
+     * Class for clients to access.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with
+     * IPC.
+     */
+    public class LocalBinder extends Binder {
+        public DataProviderService getService() {
+            return DataProviderService.this;
+        }
+    }
+	
+	private final IBinder mBinder = new LocalBinder();
+	
 	@Override
-	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public IBinder onBind(Intent intent) {
+		return mBinder;
 	}
 	
 	@Override
@@ -29,6 +57,20 @@ public class DataProviderService extends OrmLiteBaseService<DatabaseHelper> {
 		}
 		
 		return START_NOT_STICKY;
-	
 	}
+	
+	public IForecastRequestCallback onGetAllReportingAreas(){
+		try {
+			Repository<ReportingArea> repository = new AppRepository(getHelper().getConnectionSource()).ReportingAreaRepository();
+						
+			List<ReportingArea> reportingAreas = repository.queryForAll();
+			
+			return new ForecastRequestCallback(); 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 }
--- airqualitynow/src/com/quadrictech/airqualitynow/service/IDataProviderService.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/service/IDataProviderService.java
@@ -3,4 +3,5 @@ package com.quadrictech.airqualitynow.service;
 public interface IDataProviderService {
 	public void onGetAllForecasts();
 	public void onGetForecastById(int id);
+	public void onGetAllReportingAreas();
 }
