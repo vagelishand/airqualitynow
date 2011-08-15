26eac9794e571c80036624882468cce998e74d08
artgonzalez <artgonzalezjr@gmail.com>
2011-08-15 00:59:31 -0500

    eventmanager to communicate w servicehelper via events

--- airqualitynow/src/com/quadrictech/airqualitynow/presenter/ForecastListPresenter.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/presenter/ForecastListPresenter.java
@@ -3,6 +3,9 @@ package com.quadrictech.airqualitynow.presenter;
 import java.sql.SQLException;
 import java.util.List;
 
+import roboguice.event.EventManager;
+import roboguice.event.Observes;
+
 
 import android.app.AlertDialog;
 import android.content.Context;
@@ -12,13 +15,16 @@ import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.j256.ormlite.support.ConnectionSource;
 import com.quadrictech.airqualitynow.R;
 import com.quadrictech.airqualitynow.db.ForecastRepository;
 import com.quadrictech.airqualitynow.db.IForecastRepository;
+import com.quadrictech.airqualitynow.db.callback.ForecastRequestCallback;
 import com.quadrictech.airqualitynow.model.Forecast;
 import com.quadrictech.airqualitynow.presenter.util.ForecastArrayAdapter;
+import com.quadrictech.airqualitynow.service.helper.DataProviderServiceHelper;
 import com.quadrictech.airqualitynow.view.IForecastListView;
 
 public class ForecastListPresenter implements IForecastListPresenter<IForecastListView<ListView>>{
@@ -30,6 +36,8 @@ public class ForecastListPresenter implements IForecastListPresenter<IForecastLi
 	private List<Forecast> mForecasts;
 	private ConnectionSource mConnectionSource;
 	public ListViewAsyncTask asyncTask;
+	private EventManager mEventManager;
+	private DataProviderServiceHelper mDataProviderServiceHelper;
 	
 	/***
 	 * REquired for roboguice parameter injection
@@ -43,10 +51,12 @@ public class ForecastListPresenter implements IForecastListPresenter<IForecastLi
 		mRepository = repository;
 	}
 	
-	public void initialize(IForecastListView<ListView> view, ConnectionSource connectionSource) {
-		mContext = view.getView().getContext();
-		mConnectionSource = connectionSource;
-		mForecastListView = view;
+	public void initialize(PresenterInitializeParameter parameterObject) {
+		mContext = parameterObject.view.getView().getContext();
+		mConnectionSource = parameterObject.connectionSource;
+		mForecastListView = parameterObject.view;
+		mEventManager = parameterObject.eventManager;
+		mDataProviderServiceHelper = (DataProviderServiceHelper) parameterObject.dataProviderServiceHelper;
 		
 		try {
 			mRepository = new ForecastRepository(mConnectionSource);
@@ -59,8 +69,17 @@ public class ForecastListPresenter implements IForecastListPresenter<IForecastLi
 	}
 
 	public void initializeList(){
-		asyncTask = new ListViewAsyncTask();
-		asyncTask.execute();
+		//asyncTask = new ListViewAsyncTask();
+		//asyncTask.execute();
+		
+		mDataProviderServiceHelper.getAllForecasts();
+	}
+	
+	protected void handleGetAllForecasts(@Observes ForecastRequestCallback callback){
+		Toast.makeText(mContext, "You won't regret it!", Toast.LENGTH_LONG).show();
+		//mForecasts = callback.getForecasts();
+		//mAdapter = new ForecastArrayAdapter(mContext, R.layout.forecastlistrow, mForecasts);
+		//mForecastListView.getView().setAdapter(mAdapter);
 	}
 
 	public void onDestroy() {
