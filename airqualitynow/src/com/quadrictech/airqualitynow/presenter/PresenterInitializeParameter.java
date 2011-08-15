f92c3d73e9e1ec50fb2be9d3c7d98c20375d0890
artgonzalez <artgonzalezjr@gmail.com>
2011-08-15 10:36:17 -0500

    member updates no need for connectionsource

--- airqualitynow/src/com/quadrictech/airqualitynow/presenter/PresenterInitializeParameter.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/presenter/PresenterInitializeParameter.java
@@ -3,20 +3,17 @@ package com.quadrictech.airqualitynow.presenter;
 import roboguice.event.EventManager;
 import android.widget.ListView;
 
-import com.j256.ormlite.support.ConnectionSource;
 import com.quadrictech.airqualitynow.service.helper.DataProviderServiceHelper;
 import com.quadrictech.airqualitynow.view.IForecastListView;
 
 public class PresenterInitializeParameter {
 	public IForecastListView<ListView> view;
-	public ConnectionSource connectionSource;
 	public EventManager eventManager;
 	public DataProviderServiceHelper dataProviderServiceHelper;
 
 	public PresenterInitializeParameter(IForecastListView<ListView> view,
-			ConnectionSource connectionSource, EventManager eventManager, DataProviderServiceHelper helper) {
+			EventManager eventManager, DataProviderServiceHelper helper) {
 		this.view = view;
-		this.connectionSource = connectionSource;
 		this.eventManager = eventManager;
 		this.dataProviderServiceHelper = helper;
 	}
