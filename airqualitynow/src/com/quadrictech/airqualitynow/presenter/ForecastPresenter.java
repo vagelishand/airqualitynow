fa2527d1081f086963389f240bf4307df6c02bce
artgonzalez <artgonzalezjr@gmail.com>
2011-08-16 16:16:31 -0500

    setting up presenter

--- airqualitynow/src/com/quadrictech/airqualitynow/presenter/ForecastPresenter.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/presenter/ForecastPresenter.java
@@ -3,19 +3,43 @@ package com.quadrictech.airqualitynow.presenter;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.view.View;
+import roboguice.event.Observes;
 
+import android.content.Context;
+import android.view.View;
+import android.widget.Toast;
+
+import com.quadrictech.airqualitynow.event.BindedToServiceEvent;
 import com.quadrictech.airqualitynow.model.Forecast;
+import com.quadrictech.airqualitynow.service.helper.IRemoteDataProviderServiceHelper;
 import com.quadrictech.airqualitynow.view.IForecastView;
 
 public class ForecastPresenter implements IForecastPresenter<IForecastView<View>> {
 
 	private IForecastView<View> mForecastView;
+	private IRemoteDataProviderServiceHelper mRemoteDataProviderServiceHelper;
+	private Context mContext;
 	
-	public void initialize(IForecastView<View> view) {
+	public ForecastPresenter(){
+		
+	}
+	
+	public ForecastPresenter(IForecastView<View> view, IRemoteDataProviderServiceHelper dataProviderServiceHelper){
 		mForecastView = view;
+		mRemoteDataProviderServiceHelper = dataProviderServiceHelper;
+	}
+	
+	public void initialize(PresenterInitializeParameter parameterObject) {
+		mContext = parameterObject.view.getView().getContext();
+		mForecastView = parameterObject.view;
+		mRemoteDataProviderServiceHelper = parameterObject.remoteDataProviderServiceHelper;
 		setForecastTableValues();
 	}
+	
+	public void initializeTable(@Observes BindedToServiceEvent event){
+		Toast.makeText(mContext,"retrieving observd", Toast.LENGTH_SHORT).show();
+		mRemoteDataProviderServiceHelper.getObservedByZipCode("78756");
+	} 
 
 	public void onDestroy() {
 		mForecastView.onDestroy();		
--- airqualitynow/src/com/quadrictech/airqualitynow/presenter/IForecastPresenter.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/presenter/IForecastPresenter.java
@@ -1,9 +1,5 @@
 package com.quadrictech.airqualitynow.presenter;
 
-import android.view.View;
-
-import com.quadrictech.airqualitynow.view.IForecastView;
-
 public interface IForecastPresenter<T> extends IPresenter<T> {
-	public void initialize(IForecastView<View> view);
+	public void initialize(PresenterInitializeParameter parameterObject);
 }
