22a4ecb92b6f4a2c68548f7162249fa2c1bc9ced
artgonzalez <artgonzalezjr@gmail.com>
2011-08-16 16:14:24 -0500

    setup remotedataProviderservice

--- airqualitynow/src/com/quadrictech/airqualitynow/forecast/AQIForecastActivity.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/forecast/AQIForecastActivity.java
@@ -4,15 +4,20 @@ import com.google.inject.Inject;
 
 import com.quadrictech.airqualitynow.R;
 import com.quadrictech.airqualitynow.presenter.IForecastPresenter;
+import com.quadrictech.airqualitynow.presenter.PresenterInitializeParameter;
+import com.quadrictech.airqualitynow.service.helper.RemoteDataProviderServiceHelper;
 import com.quadrictech.airqualitynow.view.IForecastView;
 
 import roboguice.activity.RoboActivity;
+import roboguice.event.EventManager;
 import android.os.Bundle;
 import android.view.View;
 
 public class AQIForecastActivity extends RoboActivity {
 	@Inject private IForecastView<View> mForecastView;
 	@Inject private IForecastPresenter<View> mForecastPresenter;
+	private RemoteDataProviderServiceHelper mProviderServiceHelper;
+	@Inject private EventManager mEventManager; 
 	
     /** Called when the activity is first created. */
     @Override
@@ -20,8 +25,10 @@ public class AQIForecastActivity extends RoboActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forecast);
         
+        mProviderServiceHelper = new RemoteDataProviderServiceHelper(mForecastView.getView().getContext(), mEventManager); 
+        
         mForecastView.initialize(mForecastPresenter);
-    	mForecastPresenter.initialize(mForecastView);
+    	mForecastPresenter.initialize(new PresenterInitializeParameter(mForecastView, mEventManager, mProviderServiceHelper));
     }
     
     @Override
