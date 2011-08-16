b74fd8ec5e8b70dfddb72b73336567801e95eb80
artgonzalez <artgonzalezjr@gmail.com>
2011-08-15 23:43:26 -0500

    modify to use correct callback

--- airqualitynow/src/com/quadrictech/airqualitynow/inet/rest/RestClient.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/inet/rest/RestClient.java
@@ -2,63 +2,47 @@ package com.quadrictech.airqualitynow.inet.rest;
 
 import java.io.IOException;
 
-import com.google.api.client.googleapis.GoogleUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.extensions.android2.AndroidHttp;
-import com.quadrictech.airqualitynow.base.callback.IRequestCallback;
+import com.quadrictech.airqualitynow.inet.callback.IRestRequestCallback;
+import com.quadrictech.airqualitynow.inet.callback.RestRequestCallback;
 
 public class RestClient {
 	/**
 	 * This should return a compatible HttpTransport for current device
 	 */
 	public static final HttpTransport TRANSPORT = (HttpTransport) AndroidHttp.newCompatibleTransport();
-	private boolean mError;
 	
 	/**
 	 * Execute Http Get requests using google apis
-	 * 
-	 * @param url Callers supply a url string with get parameters
+	 * @param parameterObject TODO
 	 * @param requestCallback Uses to process reply/error 
 	 */
-	public IRequestCallback<HttpResponse> executeHttpGet(GoogleUrl url, IRequestCallback<HttpResponse> requestCallback){
+	public  synchronized static IRestRequestCallback executeHttpGet(AirNowUrl parameterObject){
 		HttpRequest request = null;
 		HttpResponse response = null;
+		IRestRequestCallback requestCallback = null;
 		
 		try {
-			request = TRANSPORT.createRequestFactory().buildGetRequest(url);
-			
+			request = TRANSPORT.createRequestFactory().buildGetRequest(parameterObject.url);
+			requestCallback = new RestRequestCallback();
 			response = request.execute();
 			
 			requestCallback.onResponseReceived(response);
-			mError = false;
 		} catch (IOException e) {
 			requestCallback.onError(new Throwable("Error getting remote data."));
-			mError = true;
 		}
 		finally{
-			try {
-				if(response != null){
-					response = null;
+				try {
+					TRANSPORT.shutdown();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-				
-				TRANSPORT.shutdown();
-				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 		}
 		
 		return requestCallback;
-	}
-	
-	/**
-	 * This is mainly used in the Test project for Unit Testing
-	 * @return Request execute error status
-	 */
-	public boolean getErrorStatus(){
-		return mError;
 	}
 }
