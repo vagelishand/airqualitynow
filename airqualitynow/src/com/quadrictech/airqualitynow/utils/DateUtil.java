c1415a1ea1897a09a95287263b64d0b51234a61c
artgonzalez <artgonzalezjr@gmail.com>
2011-08-18 15:56:25 -0500

    add getdate

--- airqualitynow/src/com/quadrictech/airqualitynow/utils/DateUtil.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/utils/DateUtil.java
@@ -1,6 +1,8 @@
 package com.quadrictech.airqualitynow.utils;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DateUtil {
 	public static final String DATE_FORMAT = "yyyy/MM/dd HH:mm:ss a";
@@ -8,4 +10,21 @@ public class DateUtil {
 	public static SimpleDateFormat getDateFormat(String dateFormat){
 		return new SimpleDateFormat(dateFormat);
 	}
+	
+	public static Date getDate(String date, String format) throws ParseException{
+		return getDateFormat(format).parse(date);
+	}
+	
+	/**
+	 * String dateStr = "04/05/2010"; 
+ 
+SimpleDateFormat curFormater = new SimpleDateFormat("dd/MM/yyyy"); 
+Date dateObj = curFormater.parse(dateStr); 
+SimpleDateFormat postFormater = new SimpleDateFormat("MMMM dd, yyyy"); 
+ 
+String newDateStr = postFormater.format(dateObj)
+	 */
+	
 }
+
+
