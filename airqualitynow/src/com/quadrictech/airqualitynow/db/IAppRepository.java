c356f36ad5fa7075cfda3d6e1fd228bc5c65ee75
artgonzalez <artgonzalezjr@gmail.com>
2011-08-18 00:35:22 -0500

    add observedrepository

--- airqualitynow/src/com/quadrictech/airqualitynow/db/IAppRepository.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/db/IAppRepository.java
@@ -4,4 +4,5 @@ public interface IAppRepository {
 	public IStateRepository StateRepository();
 	public IForecastRepository ForecastRepository();
 	public IReportingAreaRepository ReportingAreaRepository();
+	public IObservedRepository ObservedRepository();
 }
