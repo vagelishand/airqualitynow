05a6adff68db93596f658f0b8d0a10cfb6a1f5b4
artgonzalez <artgonzalezjr@gmail.com>
2011-08-18 00:37:00 -0500

    implement initialize methods for Test mocks and add observed method by date

--- airqualitynow/src/com/quadrictech/airqualitynow/service/DataProviderService.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/service/DataProviderService.java
@@ -11,20 +11,21 @@ import com.j256.ormlite.android.apptools.OrmLiteBaseService;
 import com.quadrictech.airqualitynow.db.AppRepository;
 import com.quadrictech.airqualitynow.db.DatabaseHelper;
 import com.quadrictech.airqualitynow.db.IForecastRepository;
+import com.quadrictech.airqualitynow.db.IObservedRepository;
 import com.quadrictech.airqualitynow.db.IReportingAreaRepository;
 import com.quadrictech.airqualitynow.db.callback.ForecastRequestCallback;
 import com.quadrictech.airqualitynow.db.callback.ILocalRequestCallback;
+import com.quadrictech.airqualitynow.db.callback.ObservedRequestCallback;
 import com.quadrictech.airqualitynow.db.callback.ReportingAreaRequestCallback;
 import com.quadrictech.airqualitynow.model.Forecast;
 import com.quadrictech.airqualitynow.model.ForecastWrapper;
 import com.quadrictech.airqualitynow.model.Observed;
+import com.quadrictech.airqualitynow.model.ObservedWrapper;
 import com.quadrictech.airqualitynow.model.ReportingArea;
 import com.quadrictech.airqualitynow.model.ReportingAreaWrapper;
 import com.quadrictech.airqualitynow.model.State;
 
 public class DataProviderService extends OrmLiteBaseService<DatabaseHelper> implements IDataProviderService {
-	private IForecastRepository mForecastRepository;
-	
 	/**
      * Class for clients to access.  Because we know this service always
      * runs in the same process as its clients, we don't need to deal with
@@ -37,6 +38,9 @@ public class DataProviderService extends OrmLiteBaseService<DatabaseHelper> impl
     }
 	
 	private final IBinder mBinder = new LocalBinder();
+	private IForecastRepository mForecastRepository;
+	private IReportingAreaRepository mReportingAreaRepository;
+	private IObservedRepository mObservedRepository;
 	
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -60,9 +64,11 @@ public class DataProviderService extends OrmLiteBaseService<DatabaseHelper> impl
 	
 	public ILocalRequestCallback<ReportingArea> onGetAllReportingAreas(){
 		try {
-			IReportingAreaRepository repository = new AppRepository(getHelper().getConnectionSource()).ReportingAreaRepository();
+			if(mReportingAreaRepository == null){
+				mReportingAreaRepository = new AppRepository(getHelper().getConnectionSource()).ReportingAreaRepository();
+			}
 						
-			List<ReportingArea> reportingAreas = repository.getAll();
+			List<ReportingArea> reportingAreas = mReportingAreaRepository.getAll();
 			
 			return new ReportingAreaRequestCallback(new ReportingAreaWrapper(reportingAreas)); 
 		} catch (SQLException e) {
@@ -73,8 +79,10 @@ public class DataProviderService extends OrmLiteBaseService<DatabaseHelper> impl
 	}
 	
 	public ILocalRequestCallback<Forecast> onGetAllForecasts(){
+		if(mForecastRepository == null){
+			mForecastRepository = new AppRepository(getHelper().getConnectionSource()).ForecastRepository();
+		}
 		
-		mForecastRepository = new AppRepository(getHelper().getConnectionSource()).ForecastRepository();
 		try {
 			List<Forecast> forecasts = mForecastRepository.getAll();
 			ILocalRequestCallback<Forecast> callback = new ForecastRequestCallback(new ForecastWrapper(forecasts));
@@ -89,7 +97,9 @@ public class DataProviderService extends OrmLiteBaseService<DatabaseHelper> impl
 	}
 	
 	public ILocalRequestCallback<Forecast> onGetForecastById(int id){
-		mForecastRepository = new AppRepository(getHelper().getConnectionSource()).ForecastRepository();
+		if(mForecastRepository == null){
+			mForecastRepository = new AppRepository(getHelper().getConnectionSource()).ForecastRepository();
+		}
 		
 		try {
 			Forecast forecast = mForecastRepository.getById(id);
@@ -104,8 +114,36 @@ public class DataProviderService extends OrmLiteBaseService<DatabaseHelper> impl
 		return null;
 	}
 
-	public ILocalRequestCallback<Observed> onGetObservedByZipCode(String zipCode) {
-		// TODO Auto-generated method stub
+	public ILocalRequestCallback<Observed> onGetObservedByDate(String date) {
+		
+		if(mObservedRepository == null){
+			mObservedRepository = new AppRepository(getHelper().getConnectionSource()).ObservedRepository();
+		}
+		
+		try {
+			List<Observed> observed = mObservedRepository.getByFieldEquals("DateObserved", date);
+			ILocalRequestCallback<Observed> callback = new ObservedRequestCallback(new ObservedWrapper(observed));
+			
+			return callback;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
 		return null;
 	}
+
+	public void initialize(IForecastRepository fr) {
+		mForecastRepository = fr;
+	}
+	
+	public void initialize( IReportingAreaRepository rar){
+		mReportingAreaRepository = rar;
+	}
+
+	public void initialize(IObservedRepository or) {
+		mObservedRepository = or;		
+	}
+	
+	
 }
--- airqualitynow/src/com/quadrictech/airqualitynow/service/IDataProviderService.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/service/IDataProviderService.java
@@ -1,5 +1,8 @@
 package com.quadrictech.airqualitynow.service;
 
+import com.quadrictech.airqualitynow.db.IForecastRepository;
+import com.quadrictech.airqualitynow.db.IObservedRepository;
+import com.quadrictech.airqualitynow.db.IReportingAreaRepository;
 import com.quadrictech.airqualitynow.db.callback.ILocalRequestCallback;
 import com.quadrictech.airqualitynow.model.Forecast;
 import com.quadrictech.airqualitynow.model.Observed;
@@ -9,5 +12,8 @@ public interface IDataProviderService {
 	public ILocalRequestCallback<Forecast> onGetAllForecasts();
 	public ILocalRequestCallback<Forecast> onGetForecastById(int id);
 	public ILocalRequestCallback<ReportingArea> onGetAllReportingAreas();
-	public ILocalRequestCallback<Observed> onGetObservedByZipCode(String zipCode);
+	public ILocalRequestCallback<Observed> onGetObservedByDate(String date);
+	public void initialize(IForecastRepository fr);
+	public void initialize(IReportingAreaRepository rar);
+	public void initialize(IObservedRepository or);
 }
