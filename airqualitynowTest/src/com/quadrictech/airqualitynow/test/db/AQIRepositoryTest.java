981c2c12cef50b8847b5516fd48206bd3df4eaae
artgonzalez <artgonzalezjr@gmail.com>
2011-08-18 00:36:20 -0500

    not used

--- airqualitynowTest/src/com/quadrictech/airqualitynow/test/db/AQIRepositoryTest.java
+++
@@ -1,9 +0,0 @@
-package com.quadrictech.airqualitynow.test.db;
-
-import com.quadrictech.airqualitynow.robo.AirQualityNowApplication;
-import roboguice.test.RoboUnitTestCase;
-
-public class AQIRepositoryTest extends RoboUnitTestCase<AirQualityNowApplication>{
-	
-
-}
