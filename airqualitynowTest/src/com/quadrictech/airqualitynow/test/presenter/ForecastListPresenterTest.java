14b84d6c0d3ef3d6fce935c01c801cc36678c466
artgonzalez <artgonzalezjr@gmail.com>
2011-08-19 01:06:43 -0500

    test handleForecasts for logic

--- airqualitynowTest/src/com/quadrictech/airqualitynow/test/presenter/ForecastListPresenterTest.java
+++ airqualitynowTest/src/com/quadrictech/airqualitynow/test/presenter/ForecastListPresenterTest.java
@@ -1,10 +1,8 @@
 package com.quadrictech.airqualitynow.test.presenter;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
-import junit.framework.Assert;
 
 import roboguice.test.RoboUnitTestCase;
 import android.test.suitebuilder.annotation.MediumTest;
@@ -12,90 +10,59 @@ import android.widget.ListView;
 
 import com.google.android.testing.mocking.AndroidMock;
 import com.google.android.testing.mocking.UsesMocks;
-import com.quadrictech.airqualitynow.db.IForecastRepository;
+import com.quadrictech.airqualitynow.db.callback.ILocalRequestCallback;
 import com.quadrictech.airqualitynow.model.Forecast;
 import com.quadrictech.airqualitynow.presenter.ForecastListPresenter;
 import com.quadrictech.airqualitynow.robo.AirQualityNowApplication;
+import com.quadrictech.airqualitynow.service.helper.IDataProviderServiceHelper;
+import com.quadrictech.airqualitynow.test.db.DataHelper;
 import com.quadrictech.airqualitynow.view.IForecastListView;
 
 public class ForecastListPresenterTest<T> extends RoboUnitTestCase<AirQualityNowApplication> {
 	
-	private ForecastListPresenter presenter;
-	private IForecastRepository repository;
-	private IForecastListView<ListView> view;
+	private ForecastListPresenter mPresenter;
+	private IForecastListView<ListView> mView;
+	private IDataProviderServiceHelper mDataProviderServiceHelper;
+	private DataHelper<Forecast> mDataHelper;
+	ILocalRequestCallback<Forecast> mCallback;
+	List<Forecast> mForecasts;
 	
 	@SuppressWarnings("unchecked")
 	public void setUp(){
-		repository = AndroidMock.createMock(IForecastRepository.class);
 				
-		view =  AndroidMock.createMock(IForecastListView.class);
-		//presenter = new ForecastListPresenter(view, repository);
+		mView =  AndroidMock.createMock(IForecastListView.class);
+		mDataHelper = new DataHelper<Forecast>(Forecast.class);
+		mCallback = AndroidMock.createMock(ILocalRequestCallback.class);
+		mForecasts = mDataHelper.getList();
+		mPresenter = new ForecastListPresenter(mView, mDataProviderServiceHelper, this.getInstrumentation().getContext());
 	}
 	
-	@UsesMocks(IForecastListView.class)
+	@UsesMocks(ILocalRequestCallback.class)
 	@MediumTest
-	public void testInitializeList() throws SQLException{
-		List<Forecast> forecasts = getForecasts();
-				
-		AndroidMock.expect(repository.getAll()).andReturn(forecasts);
-		
-		AndroidMock.replay(repository);
-		
-		/*try {
-			//presenter.initializeList();
-			presenter.asyncTask.get();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		
-		AndroidMock.verify(repository);
-	}
-	
-	@UsesMocks(IForecastListView.class)
-	@MediumTest
-	public void testOnPollutantButtonClick(){
-		try{
-			presenter.onPollutantGuideButtonClick();
-			AndroidMock.replay(view);
-			
-			presenter.onPollutantGuideButtonClick();
-			AndroidMock.verify(view);
-			Assert.fail("Should throw null Context exception");
-		}catch(NullPointerException e){
-			//
-		}
-	}
-	
-	@MediumTest
-	public void testOnSearchAreaClick(){
-		presenter.onSearchAreaClick();
-		AndroidMock.replay(view);
-		
-		AndroidMock.verify(view);
-	}
-	
-	public List<Forecast> getForecasts(){
-		
-		List<Forecast> forecasts = new ArrayList<Forecast>(){
-			private static final long serialVersionUID = 1L;
+	public void testInitializeListWhenDataReceivedFromService() throws SQLException{
 
-		{
-			add(getForecast(10));
-			add(getForecast(200));
-		}};
+		mCallback.onResponseReceived(mForecasts);		
+		AndroidMock.expect(mCallback.getErrorStatus()).andReturn(false);
+		AndroidMock.expect(mCallback.getList()).andReturn(mForecasts);
+		AndroidMock.replay(mCallback);
 		
-		return forecasts;
+		mCallback.onResponseReceived(mForecasts);
+		mPresenter.handleForecasts(mCallback);
+		AndroidMock.verify(mCallback);
 	}
 	
-	private Forecast getForecast(int aqi){
-		Forecast f = new Forecast();
-		f.AQI = aqi;
+	@MediumTest
+	public void testInitializeListWithErrorReturnedFromService(){
+		Throwable e = new Throwable("Test Error");
+		mCallback.onError(e);
+		AndroidMock.expect(mCallback.getErrorStatus()).andReturn(true);
+		AndroidMock.expect(mCallback.getErrorMessage()).andReturn("Test Error");
+		AndroidMock.replay(mCallback);
 		
-		return f;
+		mPresenter.handleForecasts(mCallback);
+		mCallback.onError(e);
+		AndroidMock.verify(mCallback);
 	}
+	
 	
 }
