2ebcdddb447a8ab5b81030a71f5c0f6bdcbd5c73
artgonzalez <artgonzalezjr@gmail.com>
2011-08-20 02:15:50 -0500

    remove service helper n startup service code from here now starting from main activity

--- airqualitynow/src/com/quadrictech/airqualitynow/reportingarea/ReportingAreaListActivity.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/reportingarea/ReportingAreaListActivity.java
@@ -4,20 +4,16 @@ import com.google.inject.Inject;
 import com.quadrictech.airqualitynow.R;
 import com.quadrictech.airqualitynow.presenter.ReportingAreaListPresenter;
 import com.quadrictech.airqualitynow.presenter.PresenterInitializeParameter;
-import com.quadrictech.airqualitynow.service.DataProviderService;
-import com.quadrictech.airqualitynow.service.helper.DataProviderServiceHelper;
 import com.quadrictech.airqualitynow.view.ReportingAreaListView;
 
 import roboguice.activity.RoboActivity;
 import roboguice.event.EventManager;
-import android.content.Intent;
 import android.os.Bundle;
 
 public class ReportingAreaListActivity extends RoboActivity {
 	@Inject private ReportingAreaListView mForecastListView;
 	@Inject private ReportingAreaListPresenter mForecastListPresenter;
 	@Inject protected EventManager mEventManager;
-	private DataProviderServiceHelper serviceHelper;
 	
     /** Called when the activity is first created. */
     @Override
@@ -25,21 +21,15 @@ public class ReportingAreaListActivity extends RoboActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reportingarealist);
         
-        Intent intent = new Intent(this, DataProviderService.class);
-        startService(intent);
-        serviceHelper = new DataProviderServiceHelper(mForecastListView.getView().getContext(), mEventManager);
-
         mForecastListView.initialize();
     	mForecastListView.mPresenter = mForecastListPresenter;
-    	mForecastListPresenter.initialize(new PresenterInitializeParameter(mForecastListView,  mEventManager, serviceHelper));
+    	mForecastListPresenter.initialize(new PresenterInitializeParameter(mForecastListView,  mEventManager));
     }
     
     @Override
     public void onDestroy(){
-    	super.onDestroy();
-    	Intent intent = new Intent(this, DataProviderService.class);
-    	stopService(intent);
     	mForecastListView.onDestroy();
     	mForecastListPresenter.onDestroy();
+    	super.onDestroy();
     }
 }
