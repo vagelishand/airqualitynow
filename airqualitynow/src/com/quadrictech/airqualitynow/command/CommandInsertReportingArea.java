3c28ed39476e40cac9edca87ab7685ac7600a123
artgonzalez <artgonzalezjr@gmail.com>
2011-08-19 22:24:20 -0500

    insert area

--- airqualitynow/src/com/quadrictech/airqualitynow/command/CommandInsertReportingArea.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/command/CommandInsertReportingArea.java
@@ -2,13 +2,13 @@ package com.quadrictech.airqualitynow.command;
 
 import com.quadrictech.airqualitynow.db.callback.ILocalRequestCallback;
 import com.quadrictech.airqualitynow.model.ReportingArea;
-import com.quadrictech.airqualitynow.service.DataProviderService;
+import com.quadrictech.airqualitynow.service.IDataProviderService;
 
 public class CommandInsertReportingArea extends DaoCommand<ILocalRequestCallback<ReportingArea>> {
-	private DataProviderService mDataProviderService;
+	private IDataProviderService mDataProviderService;
 	private ReportingArea mReportingArea;
 	
-	public CommandInsertReportingArea(DataProviderService dataProviderService, ReportingArea reportingArea){
+	public CommandInsertReportingArea(IDataProviderService dataProviderService, ReportingArea reportingArea){
 		mDataProviderService = dataProviderService;
 		mReportingArea = reportingArea;
 	}
