1da16eb09b8e40b979780561f6866b720550da32
artgonzalez <artgonzalezjr@gmail.com>
2011-08-20 02:19:02 -0500

    removing service n events

--- airqualitynow/src/com/quadrictech/airqualitynow/presenter/PresenterInitializeParameter.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/presenter/PresenterInitializeParameter.java
@@ -4,8 +4,6 @@ import roboguice.event.EventManager;
 import android.view.View;
 import android.widget.ListView;
 
-import com.quadrictech.airqualitynow.service.helper.DataProviderServiceHelper;
-import com.quadrictech.airqualitynow.service.helper.RemoteDataProviderServiceHelper;
 import com.quadrictech.airqualitynow.view.IReportingAreaListView;
 import com.quadrictech.airqualitynow.view.IForecastView;
 
@@ -13,20 +11,16 @@ public class PresenterInitializeParameter {
 	public IReportingAreaListView<ListView> listView;
 	public IForecastView<View> view;
 	public EventManager eventManager;
-	public DataProviderServiceHelper dataProviderServiceHelper;
-	public RemoteDataProviderServiceHelper remoteDataProviderServiceHelper;
 
 	public PresenterInitializeParameter(IReportingAreaListView<ListView> view,
-			EventManager eventManager, DataProviderServiceHelper helper) {
+			EventManager eventManager) {
 		this.listView = view;
 		this.eventManager = eventManager;
-		this.dataProviderServiceHelper = helper;
 	}
 	
 	public PresenterInitializeParameter(IForecastView<View> view,
-			EventManager eventManager, RemoteDataProviderServiceHelper helper) {
+			EventManager eventManager) {
 		this.view = view;
 		this.eventManager = eventManager;
-		remoteDataProviderServiceHelper = helper;
 	}
 }
