b7913e20850def75a6142acf0f0ec1fc0267211f
artgonzalez <artgonzalezjr@gmail.com>
2011-08-23 16:18:39 -0500

    contextmenu handlers

--- airqualitynow/src/com/quadrictech/airqualitynow/view/IReportingAreaListView.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/view/IReportingAreaListView.java
@@ -2,11 +2,14 @@ package com.quadrictech.airqualitynow.view;
 
 import com.quadrictech.airqualitynow.presenter.util.ReportingAreaArrayAdapter;
 
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 
 public interface IReportingAreaListView<T> extends IView<T> {
 	public void setAdapter(ReportingAreaArrayAdapter adapter);
 	public void onItemClick(AdapterView<?> parent, View view, int position, long id);
+	public boolean onMenuItemClick(MenuItem item);
+	public boolean onContextItemSelected(MenuItem item);
 	public String getEditTextString();
 }
--- airqualitynow/src/com/quadrictech/airqualitynow/view/ReportingAreaListView.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/view/ReportingAreaListView.java
@@ -4,24 +4,30 @@ import roboguice.inject.InjectView;
 
 import com.google.inject.Inject;
 import com.quadrictech.airqualitynow.R;
+import com.quadrictech.airqualitynow.model.ReportingArea;
 import com.quadrictech.airqualitynow.presenter.ReportingAreaListPresenter;
 import com.quadrictech.airqualitynow.presenter.util.ReportingAreaArrayAdapter;
 
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class ReportingAreaListView implements IReportingAreaListView<ListView>, OnItemClickListener, OnClickListener  {
+public class ReportingAreaListView implements IReportingAreaListView<ListView>, OnItemClickListener, OnClickListener, OnMenuItemClickListener  {
 	@InjectView(R.id.reportingAreasList) 					private ListView mView;
 	@InjectView(R.id.reportingAreaListTableSearchEditText) 	private EditText mSearchEditText;
 	@InjectView(R.id.reportingAreaListTableSearchButton)	private Button   mSearchButton;
 	@InjectView(R.id.reportingAreaListTableAddButton)		private Button   mAddButton;
 	@InjectView(R.id.reportingAreaListTableGuideButton)		private Button   mGuideButton;
 	@Inject 												public ReportingAreaListPresenter mPresenter;
+	
 	
 	public ReportingAreaListView(){
 		
@@ -65,5 +71,39 @@ public class ReportingAreaListView implements IReportingAreaListView<ListView>, 
 
 	public String getEditTextString() {
 		return this.mSearchEditText.getText().toString();
+	}
+
+	public boolean onMenuItemClick(MenuItem item) {
+		AdapterView.AdapterContextMenuInfo info;
+		
+		try {
+            info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+       } catch (ClassCastException e) {
+           return false;
+       }
+				
+		ReportingArea area = (ReportingArea) mView.getAdapter().getItem(info.position);
+		
+		switch (item.getItemId()) {
+	    	case Menu.FIRST: {
+	    		mPresenter.onViewForecast(area.Id);
+	    		return true;
+	    		}
+	    	case 2:{
+	    		mPresenter.onViewObserved(0);
+	    		return true;
+	    	}
+	    	case 3:{
+	    		Toast.makeText(mView.getContext(), "three", Toast.LENGTH_SHORT).show();
+	    		return true;
+	    	}
+		}
+	
+		return false;
+
+	}
+
+	public boolean onContextItemSelected(MenuItem item) {
+		return false;
 	}
 }
