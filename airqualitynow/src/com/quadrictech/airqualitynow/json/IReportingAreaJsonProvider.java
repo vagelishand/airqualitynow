705d62c31c41dd4fda59443901ef1a9e9c1689e5
artgonzalez <artgonzalezjr@gmail.com>
2011-08-23 01:58:04 -0500

    not retrieving areas remotely

--- airqualitynow/src/com/quadrictech/airqualitynow/json/IReportingAreaJsonProvider.java
+++
@@ -1,7 +0,0 @@
-package com.quadrictech.airqualitynow.json;
-
-import com.quadrictech.airqualitynow.model.IReportingAreaWrapper;
-
-public interface IReportingAreaJsonProvider extends IJsonProvider<IReportingAreaWrapper> {
-	
-}
--- airqualitynow/src/com/quadrictech/airqualitynow/model/IReportingAreaWrapper.java
+++
@@ -1,9 +0,0 @@
-package com.quadrictech.airqualitynow.model;
-
-import java.util.List;
-
-
-public interface IReportingAreaWrapper {
-	public List<ReportingArea> getReportingArea();
-	public void setReportingArea(List<ReportingArea> reportingAreas);
-}
