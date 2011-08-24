01eee2dfb320ded88e79477f916b206584f01f0d
artgonzalez <artgonzalezjr@gmail.com>
2011-08-24 12:21:55 -0500

    more utility functions

--- airqualitynow/src/com/quadrictech/airqualitynow/utils/DateUtil.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/utils/DateUtil.java
@@ -2,7 +2,11 @@ package com.quadrictech.airqualitynow.utils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
 
 public class DateUtil {
 	//public static final String DATE_FORMAT = "yyyy/MM/dd HH:mm:ss a";
@@ -16,6 +20,41 @@ public class DateUtil {
 		return getDateFormat(format).parse(date);
 	}
 	
+	public static Date getForecastIssueDate() throws ParseException{
+		Date startingDate = new Date();
+		GregorianCalendar gc = new GregorianCalendar();
+		gc.setTime(startingDate);
+		gc.add(Calendar.DAY_OF_YEAR, -1);
+		Date result = gc.getTime();		
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+		result = sdf.parse(sdf.format(result));
+		
+		return result;
+	}
+	
+	public static List<String> getNextThreeMonthDayNumberDates(Date initialDate){
+		List<String> dates = new ArrayList<String>();
+		dates.add(getDateFormat("M/dd").format(initialDate));
+		
+		GregorianCalendar gc = new GregorianCalendar();
+		gc.setTime(initialDate);
+		gc.add(Calendar.DAY_OF_YEAR, 1);
+		Date result = gc.getTime();
+		dates.add(getDateFormat("M/dd").format(result));
+		
+		gc.setTime(result);
+		gc.add(Calendar.DAY_OF_YEAR, 1);
+		result = gc.getTime();
+		dates.add(getDateFormat("M/dd").format(result));
+		
+		gc.setTime(result);
+		gc.add(Calendar.DAY_OF_YEAR, 1);
+		result = gc.getTime();
+		dates.add(getDateFormat("M/dd").format(result));
+		
+		return dates;
+	}
 	/**
 	 * String dateStr = "04/05/2010"; 
  
