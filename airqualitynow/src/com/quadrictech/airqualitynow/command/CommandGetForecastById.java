01f564cdff5a75f8c7e07177acfbdbbf2fecdeba
artgonzalez <artgonzalezjr@gmail.com>
2011-08-24 12:12:33 -0500

    add date to query

--- airqualitynow/src/com/quadrictech/airqualitynow/command/CommandGetForecastById.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/command/CommandGetForecastById.java
@@ -1,19 +1,23 @@
 package com.quadrictech.airqualitynow.command;
 
+import java.util.Date;
+
 import com.quadrictech.airqualitynow.db.callback.IDataRequestCallback;
 import com.quadrictech.airqualitynow.model.Forecast;
 import com.quadrictech.airqualitynow.service.IDataProviderService;
 
 public class CommandGetForecastById extends DaoCommand<IDataRequestCallback<Forecast>> {
 	private int mForecastId;
+	private Date mIssueDate;
 	
-	public CommandGetForecastById(int forecastId, IDataProviderService dataProviderService){
+	public CommandGetForecastById(int forecastId, Date issueDate, IDataProviderService dataProviderService){
+		mIssueDate = issueDate;
 		mForecastId = forecastId;
 		mDataProviderService = dataProviderService;
 	}
 	
 	public IDataRequestCallback<Forecast> execute() {
-		return mDataProviderService.onGetForecastByReportingAreaId(mForecastId);
+		return mDataProviderService.onGetForecastByReportingAreaId(mForecastId, mIssueDate);
 	}
 
 }
