fd4a2c5696975f7022e1248a19d6da48ba98f330
artgonzalez <artgonzalezjr@gmail.com>
2011-08-24 16:09:36 -0500

    new constructor for Observedview

--- airqualitynow/src/com/quadrictech/airqualitynow/presenter/PresenterInitializeParameter.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/presenter/PresenterInitializeParameter.java
@@ -3,12 +3,14 @@ package com.quadrictech.airqualitynow.presenter;
 import android.view.View;
 import android.widget.ListView;
 
+import com.quadrictech.airqualitynow.view.IObservedView;
 import com.quadrictech.airqualitynow.view.IReportingAreaListView;
 import com.quadrictech.airqualitynow.view.IForecastView;
 
 public class PresenterInitializeParameter {
 	public IReportingAreaListView<ListView> listView;
-	public IForecastView<View> view;
+	public IForecastView<View> forecastView;
+	public IObservedView<View> observedView;
 	public int reportingAreaId;
 	
 	public PresenterInitializeParameter(IReportingAreaListView<ListView> view) {
@@ -16,7 +18,12 @@ public class PresenterInitializeParameter {
 	}
 	
 	public PresenterInitializeParameter(IForecastView<View> view, int reportingAreaId) {
-		this.view = view;
+		this.forecastView = view;
+		this.reportingAreaId = reportingAreaId;
+	}
+	
+	public PresenterInitializeParameter(IObservedView<View> observedView, int reportingAreaId){
+		this.observedView = observedView;
 		this.reportingAreaId = reportingAreaId;
 	}
 }
