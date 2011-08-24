bd13051d6df40cfb434508de29d76d87db42449d
artgonzalez <artgonzalezjr@gmail.com>
2011-08-24 16:06:32 -0500

    initial import for retrieving multi lists to observed view

---
+++ airqualitynow/src/com/quadrictech/airqualitynow/model/viewmodel/ObservedAndForecast.java
@@ -0,0 +1,12 @@
+package com.quadrictech.airqualitynow.model.viewmodel;
+
+import java.util.List;
+
+import com.quadrictech.airqualitynow.model.Forecast;
+import com.quadrictech.airqualitynow.model.Observed;
+
+public class ObservedAndForecast {
+	public List<Observed> ObservedList;
+	public List<Forecast> Forecasts;
+	
+}
