d6bb2eed262c82dad462270ee8d0c139b909c962
artgonzalez <artgonzalezjr@gmail.com>
2011-08-24 00:20:50 -0500

    implement

--- airqualitynow/src/com/quadrictech/airqualitynow/command/CommandGetForecastById.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/command/CommandGetForecastById.java
@@ -3,18 +3,17 @@ package com.quadrictech.airqualitynow.command;
 import com.quadrictech.airqualitynow.db.callback.IDataRequestCallback;
 import com.quadrictech.airqualitynow.model.Forecast;
 import com.quadrictech.airqualitynow.service.IDataProviderService;
-//TODO determine if needed or not
+
 public class CommandGetForecastById extends DaoCommand<IDataRequestCallback<Forecast>> {
-	//private int mForecastId;
+	private int mForecastId;
 	
 	public CommandGetForecastById(int forecastId, IDataProviderService dataProviderService){
-		//mForecastId = forecastId;
-		//mDataProviderService = dataProviderService;
+		mForecastId = forecastId;
+		mDataProviderService = dataProviderService;
 	}
 	
 	public IDataRequestCallback<Forecast> execute() {
-		//return mDataProviderService.onGetForecastById(mForecastId);
-		return null;
+		return mDataProviderService.onGetForecastByReportingAreaId(mForecastId);
 	}
 
 }
