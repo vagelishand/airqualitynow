170cca8e2461eb5a1a1aa163314477cf30cca780
artgonzalez <artgonzalezjr@gmail.com>
2011-08-24 12:22:34 -0500

    add handleOnCreateContextMenu to add dynamic reporting area name to menu

--- airqualitynow/src/com/quadrictech/airqualitynow/view/IReportingAreaListView.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/view/IReportingAreaListView.java
@@ -2,8 +2,10 @@ package com.quadrictech.airqualitynow.view;
 
 import com.quadrictech.airqualitynow.presenter.util.ReportingAreaArrayAdapter;
 
+import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.AdapterView;
 
 public interface IReportingAreaListView<T> extends IView<T> {
@@ -11,5 +13,6 @@ public interface IReportingAreaListView<T> extends IView<T> {
 	public void onItemClick(AdapterView<?> parent, View view, int position, long id);
 	public boolean onMenuItemClick(MenuItem item);
 	public boolean onContextItemSelected(MenuItem item);
+	public void handleOnCreateContextMenu(ContextMenu menu, View view, ContextMenuInfo menuInfo);
 	public String getEditTextString();
 }
