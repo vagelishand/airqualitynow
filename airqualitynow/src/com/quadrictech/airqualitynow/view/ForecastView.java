b4f5a49147736b1e26a85ef424a2b16192532071
artgonzalez <artgonzalezjr@gmail.com>
2011-08-24 12:42:20 -0500

    remove comment at bottom

--- airqualitynow/src/com/quadrictech/airqualitynow/view/ForecastView.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/view/ForecastView.java
@@ -186,33 +186,3 @@ public class ForecastView implements IForecastView<View>, OnClickListener {
 		return returnList;
 	}
 }
-
-
-/**
- * 	@InjectView(R.id.observedTableCurrentLabelTextView)			public TextView currentTextView;
-	@InjectView(R.id.observedTableCurrentAQITimeDescTextView)	public TextView currentAQITimeDescTextView;
-	@InjectView(R.id.observedTableCurrentAQITextView)			public TextView currentAQITextView;
-	@InjectView(R.id.forecastTableTodayAQITextView)				public TextView todayAQITextView;
-	@InjectView(R.id.forecastTableTomorrowAQIDescTextView)		public TextView todayAQIDescTextView;
-	@InjectView(R.id.forecastTableTomorrowAQITextView)			public TextView tomorrowAQITextView;
-	@InjectView(R.id.forecastTableTomorrowAQIDescTextView)		public TextView tomorrowAQIDescTextView;
-	@InjectView(R.id.forecastTableTodayMsgTextView)				public TextView todayMsgTextView;
-	@InjectView(R.id.forecastTableTomorrowMsgTextView)			public TextView tomorrowMsgTextView;
-	@InjectView(R.id.forecastTableTomorrowActionTextView)		public TextView tomorrowActionTextView;
-	@InjectView(R.id.forecastTableTodayPollutantDetailTextView)	public TextView todayPollutantDetailTextView;
-	@InjectView(R.id.forecastTableTomorrowPollutDetailTextView)	public TextView tomorrowPollutDetailTextView;
-	@InjectView(R.id.forecastTableTodayPM25TextView)			public TextView todayPM25TextView;
-	@InjectView(R.id.forecastTableTodayPM25AQITextView)			public TextView todayPM25AQITextView;
-	@InjectView(R.id.forecastTableTodayPM25AQIDescTextView)		public TextView todayPM25AQIDescTextView;
-	@InjectView(R.id.forecastTableTomorrowPM25TextView)			public TextView tomorrowPM25TextView;
-	@InjectView(R.id.forecastTableTomorrowPM25AQITextView)		public TextView tomorrowPM25AQITextView;
-	@InjectView(R.id.forecastTableTomorrowPM25AQIDescTextView)	public TextView tomorrowPM25AQIDescTextView;
-	@InjectView(R.id.forecastTableTodayOzoneTextView)			public TextView todayOzoneTextView;
-	@InjectView(R.id.forecastTableTodayOzoneAQITextView)		public TextView todayOzoneAQITextView;
-	@InjectView(R.id.forecastTableTodayOzoneAQIDescTextView)	public TextView todayOzoneAQIDescTextView;
-	@InjectView(R.id.forecastTableTomorrowOzoneTextView)		public TextView tomorrowOzoneTextView;
-	@InjectView(R.id.forecastTableTomorrowOzoneAQITextView)		public TextView tomorrowOzoneAQITextView;
-	@InjectView(R.id.forecastTableTomorrowOzoneAQIDescTextView)	public TextView tomorrowOzoneAQIDescTextView;
-
- * /
- */
