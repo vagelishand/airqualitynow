ddfaa2a51697dcafdfdd97b003081d34c945b514
artgonzalez <artgonzalezjr@gmail.com>
2011-08-25 22:30:36 -0500

    rename to observation...

--- airqualitynow/src/com/quadrictech/airqualitynow/model/IObservedWrapper.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/model/IObservedWrapper.java
@@ -3,6 +3,6 @@ package com.quadrictech.airqualitynow.model;
 import java.util.List;
 
 public interface IObservedWrapper {
-	public List<Observed> getObserved();
-	public void setObserved(List<Observed> observed);
+	public List<Observation> getObserved();
+	public void setObserved(List<Observation> observed);
 }
