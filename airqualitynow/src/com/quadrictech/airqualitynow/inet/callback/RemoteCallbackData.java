3383b0d600a97c1b1dd53fc802d419dd3da76222
artgonzalez <artgonzalezjr@gmail.com>
2011-08-25 22:35:10 -0500

    Rename

--- airqualitynow/src/com/quadrictech/airqualitynow/inet/callback/RemoteCallbackData.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/inet/callback/RemoteCallbackData.java
@@ -3,11 +3,11 @@ package com.quadrictech.airqualitynow.inet.callback;
 import java.util.List;
 
 import com.quadrictech.airqualitynow.model.Forecast;
-import com.quadrictech.airqualitynow.model.Observed;
+import com.quadrictech.airqualitynow.model.Observation;
 import com.quadrictech.airqualitynow.model.ReportingArea;
 
 public class RemoteCallbackData {
 	public ReportingArea reportingArea;
 	public List<Forecast> forecasts;
-	public List<Observed> observed;
+	public List<Observation> observations;
 }
