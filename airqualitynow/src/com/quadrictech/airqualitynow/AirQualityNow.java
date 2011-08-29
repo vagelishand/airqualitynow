8a75336a52394ae07d83557aa31bf535fdbb253f
artgonzalez <artgonzalezjr@gmail.com>
2011-08-29 15:44:18 -0500

    schedule service

--- airqualitynow/src/com/quadrictech/airqualitynow/AirQualityNow.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/AirQualityNow.java
@@ -9,16 +9,21 @@ import com.quadrictech.airqualitynow.service.helper.RemoteDataProviderServiceHel
 
 import roboguice.activity.RoboActivity;
 import roboguice.inject.InjectView;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class AirQualityNow extends RoboActivity implements OnClickListener{
     /** Called when the activity is first created. */
 	@InjectView(R.id.mainTableObservedForecastButton) Button mButton;
 	@InjectView(R.id.mainTableForecastListButton)		Button mFButton;
+	@InjectView(R.id.mainTableMapPointsButton)			Button mPButton;
 	private IDataProviderServiceHelper mDataProviderServiceHelper;
 	private IRemoteDataProviderServiceHelper mRemoteDataProviderServiceHelper;
 	
@@ -39,6 +44,7 @@ public class AirQualityNow extends RoboActivity implements OnClickListener{
         
         mButton.setOnClickListener(this);
         mFButton.setOnClickListener(this);
+        mPButton.setOnClickListener(this);
     }
     
     @Override
@@ -53,6 +59,18 @@ public class AirQualityNow extends RoboActivity implements OnClickListener{
     	super.onResume();
     	mDataProviderServiceHelper.doBindService(this);
     	mRemoteDataProviderServiceHelper.doBindService(this);
+    	
+	    AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+	    Intent i = new Intent(this, RemoteDataProviderService.class);
+	    i.putExtra("FromAlarmManager", true);
+	    PendingIntent pi = PendingIntent.getService(this, 0, i, 0);
+	    am.cancel(pi);
+    	    
+	    am.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+	        SystemClock.elapsedRealtime() + 30*1000,
+	        30*1000, pi);
+	    
+	    Toast.makeText(this, "scheduled service", Toast.LENGTH_SHORT).show();
     }
     
     @Override
@@ -70,6 +88,14 @@ public class AirQualityNow extends RoboActivity implements OnClickListener{
 		else if(view.getId() == R.id.mainTableForecastListButton){
 			Intent intent = new Intent(AirQualityNow.this, ReportingAreaListActivity.class);
 			startActivity(intent);
+		}
+		else{
+			AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+		    Intent i = new Intent(this, RemoteDataProviderService.class);
+		    
+		    i.putExtra("FromAlarmManager", true);
+		    PendingIntent pi = PendingIntent.getService(this, 0, i, 0);
+		    am.cancel(pi);
 		}
 	}
 }
