93fbdbb9b62e466b54a739bef218212e11dc86b4
artgonzalez <artgonzalezjr@gmail.com>
2012-08-16 23:05:04 -0500

    refactor inner classes to own file

--- airqualitynow/src/com/quadrictech/airqualitynow/presenter/ObservationPresenter.java
+++ airqualitynow/src/com/quadrictech/airqualitynow/presenter/ObservationPresenter.java
@@ -1,11 +1,8 @@
 package com.quadrictech.airqualitynow.presenter;
 
 import java.text.ParseException;
-
-import com.quadrictech.airqualitynow.R;
-import com.quadrictech.airqualitynow.db.callback.IDataRequestCallback;
 import com.quadrictech.airqualitynow.model.Observation;
-import com.quadrictech.airqualitynow.presenter.util.IGuiRunnable;
+import com.quadrictech.airqualitynow.presenter.handlers.ObservationGetById;
 import com.quadrictech.airqualitynow.presenter.util.ObservedArrayAdapter;
 import com.quadrictech.airqualitynow.service.helper.DataProviderServiceHelper;
 import com.quadrictech.airqualitynow.utils.DateUtil;
@@ -13,13 +10,13 @@ import com.quadrictech.airqualitynow.view.IObservationView;
 
 import android.content.Context;
 import android.widget.ListView;
-import android.widget.Toast;
 
 public class ObservationPresenter implements IObservationPresenter<IObservationView<ListView>> {
-	private Context mContext;
-	private IObservationView<ListView> mObservedView;
-	private int mCurrentReportingAreaId;
-	private ObservedArrayAdapter mArrayAdapter;
+	public  Context mContext;
+	public IObservationView<ListView> mObservedView;
+	public int mCurrentReportingAreaId;
+	public String mZipCode;
+	public ObservedArrayAdapter mArrayAdapter;
 	
 	public ObservationPresenter(){
 		
@@ -29,52 +26,23 @@ public class ObservationPresenter implements IObservationPresenter<IObservationV
 		mContext = parameterObject.observedView.getView().getContext();
 		mObservedView = parameterObject.observedView;
 		mCurrentReportingAreaId = parameterObject.reportingAreaId;
+		mZipCode = parameterObject.zipCode;
 		initializeTable();
 	}
 	
 	public void initializeTable() {
 		try {
-			DataProviderServiceHelper.getInstance().getObservationsByReportingAreaId(mCurrentReportingAreaId, DateUtil.getDateObserved(), new HandleGetObservedById());
+			DataProviderServiceHelper.getInstance().getObservationsByReportingAreaId(mCurrentReportingAreaId, 
+					                                                                 DateUtil.getDateObserved(), 
+					                                                                 new ObservationGetById(this));
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}	
 	}
-	
-	class HandleGetObservedById implements IGuiRunnable<IDataRequestCallback<Observation>>{
-		IDataRequestCallback<Observation> callback;
 		
-		public void run() {
-			if(callback.getErrorStatus()){
-				Toast.makeText(mContext, callback.getErrorMessage(), Toast.LENGTH_SHORT).show();
-			}
-			else{
-				try{
-					if(callback.getList().size() > 0){
-						onSetObservationTableValues(callback.getList().get(0));
-					}
-					mArrayAdapter = new ObservedArrayAdapter(mContext, R.id.observationList, callback.getList());
-					
-					mObservedView.setAdapter(mArrayAdapter);
-				}
-				catch(ParseException e){
-					Toast.makeText(mContext, "Error parsing Date", Toast.LENGTH_SHORT).show();
-				}
-				
-				
-			}			
-		}
-
-		@SuppressWarnings("unchecked")
-		public void setCallback(IDataRequestCallback<?> callback) {
-			this.callback = (IDataRequestCallback<Observation>) callback;
-		}
-		
-	}
-	
 	public void onDestroy() {
 		// TODO Auto-generated method stub
-		
 	}
 
 	public void onSetObservationTableValues(Observation observation) throws ParseException {
